ccb4526dfcb8c957061f1ff1652208cd959dd0f6
Gustavo Pereira <gustavo.pereira.ext@valtech.com>
2022-06-07 20:50:49 +0200

    Fixing test cases

--- src/test/index.spec.js
+++ src/test/index.spec.js
@@ -1,9 +1,14 @@
 import { shallowMount } from '@vue/test-utils'
+import { createTestingPinia } from '@pinia/testing'
 import IndexPage from '@/src/pages/index.vue'
 
 describe('Index Page', () => {
   test('is a Vue instance', () => {
-    const wrapper = shallowMount(IndexPage)
+    const wrapper = shallowMount(IndexPage, {
+      global: {
+        plugins: [createTestingPinia()],
+      },
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
