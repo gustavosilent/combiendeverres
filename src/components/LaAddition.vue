8a0ed7a0e52cee227f3ece7bbf99deed99ff8cbd
Gustavo Pereira <gustavo.pereira.ext@valtech.com>
2022-06-07 19:32:54 +0200

    User select none

--- src/components/LaAddition.vue
+++ src/components/LaAddition.vue
@@ -1,12 +1,9 @@
 <template>
-  <div class="text-center">
+  <div class="text-center select-none">
     <div class="mb-4">
       <div>
-        <img
-          class="m-auto w-32 md:w-42 lg:w-48 pointer-events-none"
-          src="argent.png"
-        />
-        <h1 class="text-2xl mt-4 pointer-events-none">
+        <img class="m-auto w-32 md:w-42 lg:w-48" src="argent.png" />
+        <h1 class="text-2xl mt-4">
           Vous doit payer {{ store.getCount }} verre(s)
         </h1>
       </div>
--- src/components/OneMore.vue
+++ src/components/OneMore.vue
@@ -1,13 +1,10 @@
 <template>
-  <div class="text-center">
+  <div class="text-center select-none">
     <div class="mb-4">
       <div v-longpress="addValue" @touchstart="touchStart">
-        <img
-          class="m-auto w-32 md:w-42 lg:w-48 pointer-events-none"
-          src="cloche.png"
-        />
-        <h1 class="text-2xl mt-4 pointer-events-none">{{ units }} verre(s)</h1>
-        <span class="text-xs mt-4 text-gray-400 pointer-events-none">{{
+        <img class="m-auto w-32 md:w-42 lg:w-48" src="cloche.png" />
+        <h1 class="text-2xl mt-4">{{ units }} verre(s)</h1>
+        <span class="text-xs mt-4 text-gray-400">{{
           `${
             units === 0
               ? 'Swipe(L/R) pour adjuster'
