4ade911bf5560a7d6be0ef228b33361f9af28651
Gustavo Pereira <gustavo.pereira.ext@valtech.com>
2022-06-07 11:43:49 +0200

    First PWA proposition for homescreen, icons and service workers

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid flex h-screen justify-center items-center">
+    <div class="text-center">
+      <img class="m-auto w-32 md:w-48 lg:w-64" src="icon.png" />
+      <h1 class="text-xl mt-4">Combien de Vérres?</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
