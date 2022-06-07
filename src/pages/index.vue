11e241dbb54e70476325ca49629f5335fdba9ce9
Gustavo Pereira <gustavo.pereira.ext@valtech.com>
2022-06-07 11:45:16 +0200

    Minor adjustments on texts

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="grid flex h-screen justify-center items-center">
     <div class="text-center">
       <img class="m-auto w-32 md:w-48 lg:w-64" src="icon.png" />
-      <h1 class="text-xl mt-4">Combien de Vérres?</h1>
+      <h1 class="text-xl mt-4">Combien de Verres?</h1>
     </div>
   </div>
 </template>
