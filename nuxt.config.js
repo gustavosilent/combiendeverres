2da126eec8651ab9046dd27f5b5a0bacc2fdd48b
Gustavo Pereira <gustavo.pereira.ext@valtech.com>
2022-06-07 13:08:10 +0200

    Updating with new text

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,9 +7,9 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/combiendeverres/',
-  },
+  // router: {
+  //   base: '/combiendeverres/',
+  // },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,7 @@
     <div class="text-center">
       <img class="m-auto w-32 md:w-48 lg:w-64" src="icon.png" />
       <h1 class="text-xl mt-4">Combien de Verres?</h1>
+      <span class="text-sm mt-4">Nuxt + Vuejs + PWA</span>
     </div>
   </div>
 </template>
