7613f13e91451a62d1ab08b428fb9cb945e82c2b
Gustavo Pereira <gustavo.pereira.ext@valtech.com>
2022-06-08 15:15:20 +0200

    Fixing wording on payment

--- src/components/LaAddition.vue
+++ src/components/LaAddition.vue
@@ -7,7 +7,7 @@
           src="argent.png"
         />
         <h1 class="text-xl mt-4">
-          Tu doit payer {{ store.getCount }} verre(s)
+          Tu dois payer {{ store.getCount }} verre(s)
         </h1>
       </div>
     </div>
